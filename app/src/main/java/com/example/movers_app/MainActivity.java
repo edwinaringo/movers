64229d89661ae1a5d645e99d5f746ef9ad5f3c62
Abdallah@moringa <abdallah.abdul@student.moringaschool.com>
2021-08-03 21:55:58 +0300

    login Ui half done

--- app/src/main/java/com/example/movers_app/MainActivity.java
+++ app/src/main/java/com/example/movers_app/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.movers_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = findViewById(R.id.ebutton2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ebutton2();
+            }
+
+            private void ebutton2() {
+                Log.e("TAG", "Button to comments activity");
+                Intent intent = new Intent(MainActivity.this, Sign_Up.class);
+                startActivity(intent);
+
+
+            }
+        });
     }
 }
