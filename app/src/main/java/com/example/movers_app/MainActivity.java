3b97dafac6ead6fdf646e9de006e6369d354ccb7
Jeremiah-ogutu <jeremiah.ogutu@student.moringaschool.com>
2021-08-04 08:54:13 +0300

    implement of onclick

--- app/src/main/java/com/example/movers_app/MainActivity.java
+++ app/src/main/java/com/example/movers_app/MainActivity.java
@@ -2,13 +2,39 @@ package com.example.movers_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  implements View .OnClickListener {
+    Button mLoginbutton;
+    Button mSigninbutton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mLoginbutton = (Button)findViewById(R.id.login);
+        mSigninbutton = (Button) findViewById(R.id.signin);
+
+        mLoginbutton.setOnClickListener(this);
+        mSigninbutton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == mLoginbutton){
+            startActivity(new Intent(MainActivity.this,LoginActivity.class));
+            Toast.makeText(MainActivity.this, "login to your account", Toast.LENGTH_SHORT).show();
+        }
+
+        if (v == mSigninbutton){
+            startActivity(new Intent(MainActivity.this,Sign_Up.class));
+            Toast.makeText(MainActivity.this, "create your account", Toast.LENGTH_SHORT).show();
+        }
+
     }
 }
