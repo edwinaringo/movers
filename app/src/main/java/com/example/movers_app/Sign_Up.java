a46dd7094e7bc0d296716d768088cf63dc765783
edwinaringo <tedaringo628@gmail.com>
2021-08-06 15:09:22 +0300

    Adding spinner data to firebase

--- app/src/main/java/com/example/movers_app/Sign_Up.java
+++ app/src/main/java/com/example/movers_app/Sign_Up.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -20,22 +21,34 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Sign_Up extends AppCompatActivity implements View.OnClickListener{
 
     private FirebaseAuth mAuth;
     private EditText editTextFullName, editTextEmail, editTextPassword;
     private Button signUp;
 
+    private Spinner spinner;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
-        Spinner mySpinner = (Spinner) findViewById(R.id.spinner3);
-        ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(Sign_Up.this,
-                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.type_of_user));
-        myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        mySpinner.setAdapter(myAdapter);
+        spinner = (Spinner) findViewById(R.id.spinner3);
+
+        List<String> categories = new ArrayList<>();
+        categories.add(0, "User Type");
+        categories.add("Normal User");
+        categories.add("Mover");
+
+        ArrayAdapter<String> dataAdapter;
+        dataAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, categories);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinner.setAdapter(dataAdapter);
 
 
         mAuth = FirebaseAuth.getInstance();
@@ -47,6 +60,7 @@ public class Sign_Up extends AppCompatActivity implements View.OnClickListener{
         editTextFullName = (EditText) findViewById(R.id.fullName);
         editTextEmail = (EditText) findViewById(R.id.email);
         editTextPassword = (EditText) findViewById(R.id.password);
+        spinner = (Spinner) findViewById(R.id.spinner3);
     }
 
     @Override
@@ -60,10 +74,10 @@ public class Sign_Up extends AppCompatActivity implements View.OnClickListener{
 
 
     private void signUp() {
+
         String email = editTextEmail.getText().toString().trim();
         String name = editTextFullName.getText().toString().trim();
         String password = editTextPassword.getText().toString().trim();
-
 
         if (name.isEmpty()) {
             editTextFullName.setError("Full name is required");
@@ -100,7 +114,7 @@ public class Sign_Up extends AppCompatActivity implements View.OnClickListener{
                     public void onComplete(@NonNull Task<AuthResult> task) {
 
                         if(task.isSuccessful()){
-                            User user = new User(name, email);
+                            User user = new User(name, email, spinner);
 
 
                             FirebaseDatabase.getInstance().getReference("User")
--- app/src/main/java/com/example/movers_app/User.java
+++ app/src/main/java/com/example/movers_app/User.java
@@ -1,15 +1,26 @@
 package com.example.movers_app;
 
+import android.widget.Spinner;
+
 public class User {
-    public String name, email;
+    public String name, email, spinner;
 
     public User(){
 
     }
 
-    public User(String name, String email){
+    public String getSpinner() {
+        return spinner;
+    }
 
+    public void setSpinner(String spinner) {
+        this.spinner = spinner;
+    }
+
+    public User(String name, String email, Spinner spinner) {
         this.email = email;
         this.name = name;
+        this.getSpinner();
     }
+
 }
