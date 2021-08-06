fd34b98cf5ecd15bd3e31c6844e0acb11df783e4
edwinaringo <tedaringo628@gmail.com>
2021-08-06 16:02:25 +0300

    Created sign up method for movers

--- app/src/main/java/com/example/movers_app/User.java
+++ app/src/main/java/com/example/movers_app/User.java
@@ -3,24 +3,16 @@ package com.example.movers_app;
 import android.widget.Spinner;
 
 public class User {
-    public String name, email, spinner;
+    public String name, email;
 
     public User(){
 
     }
 
-    public String getSpinner() {
-        return spinner;
-    }
 
-    public void setSpinner(String spinner) {
-        this.spinner = spinner;
-    }
-
-    public User(String name, String email, Spinner spinner) {
+    public User(String name, String email) {
         this.email = email;
         this.name = name;
-        this.getSpinner();
     }
 
 }
