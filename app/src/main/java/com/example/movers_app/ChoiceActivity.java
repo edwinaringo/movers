f7ab0954b2345de0c13eb9e537eb43d94bfb1a2c
edwinaringo <tedaringo628@gmail.com>
2021-08-07 15:50:14 +0300

    Created layouts for movers to choose prices

--- app/src/main/java/com/example/movers_app/ChoiceActivity.java
+++ app/src/main/java/com/example/movers_app/ChoiceActivity.java
@@ -2,13 +2,37 @@ package com.example.movers_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class ChoiceActivity extends AppCompatActivity {
+public class ChoiceActivity extends AppCompatActivity implements View.OnClickListener {
+    TextView tv_User;
+    TextView tv_movingCompany;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choice);
+
+        tv_movingCompany = (TextView) findViewById(R.id.tv_movingCompany);
+        tv_User = (TextView) findViewById(R.id.tv_User);
+
+        tv_User.setOnClickListener(this);
+        tv_movingCompany.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == tv_movingCompany){
+            //mProgressBar.setVisibility(View.VISIBLE);
+            startActivity(new Intent(ChoiceActivity.this,Movers_SignUp.class));
+        }
+
+        if(v == tv_User){
+            //mProgressBar.setVisibility(View.VISIBLE);
+            startActivity(new Intent(ChoiceActivity.this,Sign_Up.class));
+        }
     }
 }
--- app/src/main/java/com/example/movers_app/Movers.java
+++ app/src/main/java/com/example/movers_app/Movers.java
@@ -3,12 +3,52 @@ package com.example.movers_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 public class Movers extends AppCompatActivity {
+
+    private EditText mCompanyName,mStudioPrice, mOneBedroomPrice, mTwoBedroomPrice, mThreeBedroomPrice;
+    private Button submitPrices;
+
+    private FirebaseDatabase db = FirebaseDatabase.getInstance();
+    private DatabaseReference root = db.getReference().child("MoverPrices");
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movers);
+
+        mCompanyName = findViewById(R.id.companyName);
+        mStudioPrice = findViewById(R.id.studioPrice);
+        mOneBedroomPrice = findViewById(R.id.oneBedRoomPrice);
+        mTwoBedroomPrice = findViewById(R.id.twoBedRoomPrice);
+        mThreeBedroomPrice = findViewById(R.id.threeBedRoomPrice);
+
+        submitPrices.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String companyName = mCompanyName.getText().toString();
+                String studioPrice = mStudioPrice.getText().toString();
+                String oneBedroomPrice = mOneBedroomPrice.getText().toString();
+                String twoBedroomPrice = mTwoBedroomPrice.getText().toString();
+                String threeBedroomPrice = mThreeBedroomPrice.getText().toString();
+
+                HashMap<String, String> pricesMap = new HashMap<>();
+
+                pricesMap.put("Company Name", companyName);
+                pricesMap.put("Studio Price", studioPrice);
+                pricesMap.put("One Bedroom Price", oneBedroomPrice);
+                pricesMap.put("Two Bedroom Price", twoBedroomPrice);
+                pricesMap.put("Three Bedroom Price", threeBedroomPrice);
+            }
+        });
     }
 }
