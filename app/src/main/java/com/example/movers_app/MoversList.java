0d48a276890dc47924ecf7efbee54ae430f2b7c8
diana3664 <dianamus2017@gmaill.com>
2021-08-10 13:13:36 +0300

    add layout activity to display movers list

---
+++ app/src/main/java/com/example/movers_app/MoversList.java
@@ -0,0 +1,14 @@
+package com.example.movers_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MoversList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movers_list);
+    }
+}
