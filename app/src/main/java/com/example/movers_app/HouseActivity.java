8eab0a450e4bb17d19e7d556b45be875bbc7da5a
loop <loopinfinity@criptext.com>
2021-08-12 18:34:11 +0300

    add profile and logout menu

--- app/src/main/java/com/example/movers_app/HouseActivity.java
+++ app/src/main/java/com/example/movers_app/HouseActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -17,6 +18,11 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class HouseActivity extends AppCompatActivity implements View.OnClickListener{
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
+    }
 
     private TextView studioText, oneBedroomText, twoBedRoomText, threeBedRoomText;
     String userName;
