fbf1b43bf5961b0503eae43f2ef12d51a1b4616d
diana3664 <dianamus2017@gmaill.com>
2021-08-14 14:24:14 +0300

    change base url

--- app/src/main/java/com/example/movers_app/Constants.java
+++ app/src/main/java/com/example/movers_app/Constants.java
@@ -1,6 +1,6 @@
 package com.example.movers_app;
 
 public class Constants {
-    public static  final String MOVERS_API_BASE_URL="http://d54989401dfe.ngrok.io";
+    public static  final String MOVERS_API_BASE_URL="https://ec421caf940.ngrok.io/";
 
 }
