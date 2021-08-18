a34cd6801f77b4b6254d857f9d5698177179f780
edwinaringo <tedaringo628@gmail.com>
2021-08-18 18:58:34 +0300

    Changed the layout of the registation activities and the profile activity

--- app/src/main/java/com/example/movers_app/ChoiceActivity.java
+++ app/src/main/java/com/example/movers_app/ChoiceActivity.java
@@ -5,32 +5,33 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class ChoiceActivity extends AppCompatActivity implements View.OnClickListener {
-    TextView tv_User;
-    TextView tv_movingCompany;
+    Button btnSignUpUser;
+    Button btnSignUpMover;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choice);
 
-        tv_movingCompany = (TextView) findViewById(R.id.tv_movingCompany);
-        tv_User = (TextView) findViewById(R.id.tv_User);
+        btnSignUpMover = (Button) findViewById(R.id.signUpMover);
+        btnSignUpUser = (Button) findViewById(R.id.signUpUser);
 
-        tv_User.setOnClickListener(this);
-        tv_movingCompany.setOnClickListener(this);
+        btnSignUpUser.setOnClickListener(this);
+        btnSignUpMover.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
-        if(v == tv_movingCompany){
+        if(v == btnSignUpMover){
             //mProgressBar.setVisibility(View.VISIBLE);
             startActivity(new Intent(ChoiceActivity.this,Movers_SignUp.class));
         }
 
-        if(v == tv_User){
+        if(v == btnSignUpUser){
             //mProgressBar.setVisibility(View.VISIBLE);
             startActivity(new Intent(ChoiceActivity.this,Sign_Up.class));
         }
