aef0e629de59216f7520045cd2d3b1a0fbfbd52d
edwinaringo <tedaringo628@gmail.com>
2021-08-18 15:47:36 +0300

    Finished up on the log in layouts

--- app/src/main/java/com/example/movers_app/LoginChoice.java
+++ app/src/main/java/com/example/movers_app/LoginChoice.java
@@ -5,32 +5,33 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class LoginChoice extends AppCompatActivity implements View.OnClickListener {
-    TextView tv_User;
-    TextView tv_movingCompany;
+    Button btn_User;
+    Button btn_Mover;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choice);
 
-        tv_movingCompany = (TextView) findViewById(R.id.tv_movingCompany);
-        tv_User = (TextView) findViewById(R.id.tv_User);
+        btn_User = (Button) findViewById(R.id.signUpUser);
+        btn_Mover = (Button) findViewById(R.id.signUpMover);
 
-        tv_User.setOnClickListener(this);
-        tv_movingCompany.setOnClickListener(this);
+        btn_User.setOnClickListener(this);
+        btn_Mover.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
-        if(v == tv_movingCompany){
+        if(v == btn_Mover){
             //mProgressBar.setVisibility(View.VISIBLE);
             startActivity(new Intent(LoginChoice.this,MoversLogIn.class));
         }
 
-        if(v == tv_User){
+        if(v == btn_User){
             //mProgressBar.setVisibility(View.VISIBLE);
             startActivity(new Intent(LoginChoice.this,LoginActivity.class));
         }
